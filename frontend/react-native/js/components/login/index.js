6e9845d5b6ec3c4f5e76af73cda64eb0e3829739
ksb <rnjstjdqhd3939@gmail.com>
2017-08-26 17:38:36 +0900

    test

--- frontend/react-native/js/components/login/index.js
+++ frontend/react-native/js/components/login/index.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import { Image } from "react-native";
 import { connect } from "react-redux";
 import {
-  Container,
-  Content,
-  Item,
-  Input,
-  Button,
-  Icon,
-  View,
-  Text
+    Container,
+    Content,
+    Item,
+    Input,
+    Button,
+    Icon,
+    View,
+    Text
 } from "native-base";
 import { Field, reduxForm } from "redux-form";
 import { setUser } from "../../actions/user";
@@ -18,109 +18,137 @@ import styles from "./styles";
 const background = require("../../../images/shadow.png");
 
 const validate = values => {
-  const error = {};
-  error.email = "";
-  error.password = "";
-  var ema = values.email;
-  var pw = values.password;
-  if (values.email === undefined) {
-    ema = "";
-  }
-  if (values.password === undefined) {
-    pw = "";
-  }
-  if (ema.length < 8 && ema !== "") {
-    error.email = "too short";
-  }
-  if (!ema.includes("@") && ema !== "") {
-    error.email = "@ not included";
-  }
-  if (pw.length > 12) {
-    error.password = "max 11 characters";
-  }
-  if (pw.length < 5 && pw.length > 0) {
-    error.password = "Weak";
-  }
-  return error;
+    const error = {};
+    error.email = "";
+    error.password = "";
+    var ema = values.email;
+    var pw = values.password;
+    if (values.email === undefined) {
+        ema = "";
+    }
+    if (values.password === undefined) {
+        pw = "";
+    }
+    if (ema.length < 8 && ema !== "") {
+        error.email = "too short";
+    }
+    if (!ema.includes("@") && ema !== "") {
+        error.email = "@ not included";
+    }
+    if (pw.length > 12) {
+        error.password = "max 11 characters";
+    }
+    if (pw.length < 5 && pw.length > 0) {
+        error.password = "Weak";
+    }
+    return error;
 };
 
 class Login extends Component {
-  static propTypes = {
-    setUser: React.PropTypes.func
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ""
+    static propTypes = {
+        setUser: React.PropTypes.func
     };
-    this.renderInput = this.renderInput.bind(this);
-  }
-
-  setUser(name) {
-    this.props.setUser(name);
-  }
-  renderInput({
-    input,
-    label,
-    type,
-    meta: { touched, error, warning },
-    inputProps
-  }) {
-    var hasError = false;
-    if (error !== undefined) {
-      hasError = true;
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ""
+        };
+        this.renderInput = this.renderInput.bind(this);
     }
-    return (
-      <Item error={hasError}>
-        <Icon active name={input.name === "email" ? "person" : "unlock"} />
-        <Input
-          placeholder={input.name === "email" ? "EMAIL" : "PASSWORD"}
-          {...input}
-        />
-        {hasError
-          ? <Item style={{ borderColor: "transparent" }}>
-              <Icon active style={{ color: "red", marginTop: 5 }} name="bug" />
-              <Text style={{ fontSize: 15, color: "red" }}>{error}</Text>
+
+    test() {
+        // https://facebook.github.io/react-native/docs/network.html
+        fetch('http://localhost:8888/auth', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: 'lynas',
+                password: '123456'
+            })
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                return responseJson;
+            })
+            .catch((error) => {
+                console.error("er", error);
+            });
+    }
+    setUser(name) {
+        this.props.setUser(name);
+    }
+    renderInput({
+                    input,
+                    label,
+                    type,
+                    meta: { touched, error, warning },
+                    inputProps
+                }) {
+        var hasError = false;
+        if (error !== undefined) {
+            hasError = true;
+        }
+        return (
+            <Item error={hasError}>
+              <Icon active name={input.name === "email" ? "person" : "unlock"} />
+              <Input
+                  placeholder={input.name === "email" ? "EMAIL" : "PASSWORD"}
+                  {...input}
+              />
+                {hasError
+                    ? <Item style={{ borderColor: "transparent" }}>
+                      <Icon active style={{ color: "red", marginTop: 5 }} name="bug" />
+                      <Text style={{ fontSize: 15, color: "red" }}>{error}</Text>
+                    </Item>
+                    : <Text />}
             </Item>
-          : <Text />}
-      </Item>
-    );
-  }
-  render() {
-    return (
-      <Container>
-        <View style={styles.container}>
-          <Content>
-            <Image source={background} style={styles.shadow}>
-              <View style={styles.bg}>
-                <Field name="email" component={this.renderInput} />
-                <Field name="password" component={this.renderInput} />
-                <Button
-                  style={styles.btn}
-                  onPress={() => this.props.navigation.navigate("Home")}
-                >
-                  <Text>Login</Text>
-                </Button>
+        );
+    }
+    render() {
+        return (
+            <Container>
+              <View style={styles.container}>
+                <Content>
+                  <Image source={background} style={styles.shadow}>
+                    <View style={styles.bg}>
+                      <Field name="email" component={this.renderInput} />
+                      <Field name="password" component={this.renderInput} />
+                      <Button
+                          style={styles.btn}
+                          onPress={() => this.props.navigation.navigate("Home")}
+                      >
+                        <Text>Login</Text>
+                      </Button>
+                      <Button
+                          style={styles.btn}
+                          onPress={() => this.test()}
+                      >
+                        <Text>Test</Text>
+                      </Button>
+                    </View>
+                  </Image>
+                </Content>
               </View>
-            </Image>
-          </Content>
-        </View>
-      </Container>
-    );
-  }
+            </Container>
+        );
+    }
 }
 const LoginSwag = reduxForm(
-  {
-    form: "test",
-    validate
-  },
-  function bindActions(dispatch) {
-    return {
-      setUser: name => dispatch(setUser(name))
-    };
-  }
+    {
+        form: "test",
+        validate
+    },
+    function bindActions(dispatch) {
+        return {
+            setUser: name => dispatch(setUser(name))
+        };
+    }
 )(Login);
 LoginSwag.navigationOptions = {
-  header: null
+    header: null
 };
 export default LoginSwag;
